94db839ce7e387d5b52f25fd8b2f0165507d5eab
armin momeni <rmin.momeni@gmail.com>
2023-05-05 17:17:03 +0430

    www and android

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {ToastController} from "@ionic/angular";
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,40 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  totalPrice = 0;
+  profitPercents = 0;
+  firstPayment = 0;
+  paymentCounts = 0;
+  monthlyPayment = 0;
 
-  constructor() {}
+  constructor(private toastController: ToastController) {
+  }
 
+  calc() {
+    if (!this.totalPrice || !this.profitPercents || !this.paymentCounts) {
+      this.presentToast('وارد کردن مبلغ کل، درصد سود و تعداد اقساط اجباری میباشد.');
+      return;
+    }
+
+    if (+this.totalPrice < +this.firstPayment) {
+      this.presentToast('مبلغ کل نمیتواند کمتر از پرداخت اولیه باشد');
+      return;
+    }
+    let remain = this.totalPrice - this.firstPayment;
+    let res = remain * this.profitPercents / 100;
+    res *= this.paymentCounts;
+    res += remain;
+    res /= this.paymentCounts;
+    this.monthlyPayment = res;
+  }
+
+  async presentToast(message: string) {
+    const toast = await this.toastController.create({
+      message: message,
+      duration: 2000,
+      position: 'top',
+      color: 'danger'
+    });
+    await toast.present();
+  }
 }
